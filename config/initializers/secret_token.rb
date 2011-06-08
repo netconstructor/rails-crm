d9f87ad9aae2c039df1cfd70960f6720e36d3343
Mike Stok <mike@stok.ca>
2011-06-08 19:14:01 -0400

    Initial commit - new rails 3.0.8 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCrm2::Application.config.secret_token = 'c3f8a133fecbe5debc8770ef2592b17f607078777b152c76dbc22ccbb80f489f846c312d0029bf1311a61e7807c840ee3e7e0eb86cff8756cb816d69c5d5e388'
