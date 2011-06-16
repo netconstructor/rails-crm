a28c23dd2072987bc188011385a8d2b4bb4e67d4
Mike Stok <mike@stok.ca>
2011-06-16 12:52:14 -0400

    Adds a decorator to Event s.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
   def index
-    @my_events = Event.assigned_to(1)
-    @unassigned_events = Event.unassigned
+    @my_events = Event.assigned_to(1).collect do |e|
+      EventDecorator.new(e)
+    end
+
+    @unassigned_events = Event.unassigned.collect do |e|
+      EventDecorator.new(e)
+    end
   end
 end
